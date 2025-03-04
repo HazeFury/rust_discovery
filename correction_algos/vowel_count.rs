b35502501fbbc4dab747fac46505625fd6729b94
HazeFury <mab.dev69@gmail.com>
2025-03-04 17:51:04 +0100

    added instructions

--- correction_algos/vowel_count.rs
+++ correction_algos/vowel_count.rs
@@ -1,3 +1,21 @@
+// Énoncé :
+// Écris une fonction qui prend une chaîne de caractères en entrée et retourne le nombre de voyelles (a, e, i, o, u) qu’elle contient.
+
+// Exemples :
+// count_vowels("hello") ➞ 2  
+// count_vowels("rust") ➞ 1  
+// count_vowels("xyz") ➞ 0  
+// count_vowels("aeiou") ➞ 5  
+
+// Instructions :
+
+// La fonction doit retourner un entier (usize).
+// Les voyelles sont : a, e, i, o, u (uniquement en minuscule, pas besoin de gérer les majuscules pour l’instant).
+// Tu peux utiliser une boucle (for), un if ou un **match` pour vérifier chaque caractère.
+
+// ----------------------------------------------------------------------------
+
+
 fn count_vowels(input: &str) -> usize {
     
     let mut result : usize = 0;
